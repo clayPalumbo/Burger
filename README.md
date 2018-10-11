9683795343b0e4a251f8e696dfe81d57a3c3ee68
Clay Palumbo <claypalumbo@clays-mbp.home>
2018-10-11 19:54:59 -0400

    init upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Burger
+I created a burger logger with MySQL, Node, Express, Handlebars and a homemade ORM. Be sure to follow the MVC design pattern; use Node and MySQL to query and route data in your app, and Handlebars to generate your HTML.
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+--Seeds file to populate database with some starter data--
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Tasty Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Good Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Good Good Burger", true);
